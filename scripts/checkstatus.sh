770865c4372ec01792819be703c73bffb456d37c
Julian Benavides <julianbenavides@gmail.com>
2018-04-19 02:24:42 -0700

    Update checkstatus.sh

--- scripts/checkstatus.sh
+++ scripts/checkstatus.sh
@@ -6,7 +6,17 @@ echo Requesting http headers...
 echo Response: $(curl --write-out %{http_code} --silent --output /dev/null https://jbits.ca/work/boeing/$1/)
 echo
 echo Starting automatic POST test to ensure that the microservice is working... 
-echo using POST name [BoeingCandidate]
+echo using POST name [$2]
 #echo curl --data "name=BoeingCandidate" https://jbits.ca/work/boeing/$1/
-RESULT=$(curl -d "name=BoeingCandidate" -X POST https://jbits.ca/work/boeing/$1/)
+RESULT=$(curl -d "name=$2" -X POST https://jbits.ca/work/boeing/$1/)
 echo "$RESULT"
+#Automatically find the POST name
+flag=`echo $RESULT|awk '{print match($0,"$2")}'`;
+
+if [ $flag -gt 0 ];then
+
+    echo "Success";
+else
+    echo "fail";
+
+fi
