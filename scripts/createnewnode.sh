fa3b385e50367fecd91be1f19076f56507d5ae71
Julian Benavides <julianbenavides@gmail.com>
2018-04-19 03:00:17 -0700

    Update createnewnode.sh

--- scripts/createnewnode.sh
+++ scripts/createnewnode.sh
@@ -2,8 +2,8 @@
 echo CREATING NEW BOEING NODE
 echo Connecting through encrypted channel using ftps and tls 1.2.
 echo
-curl --ftp-ssl-reqd -k  --ftp-create-dirs  ftp.jbits.ca/boeing$1  -u boeingnewnode@jbits.ca:boeing
-
+#curl --ftp-ssl-reqd -k  --ftp-create-dirs  ftp.jbits.ca/boeing$1  -u boeingnewnode@jbits.ca:boeing
+curl --ftp-ssl-reqd -k ftp://boeingnewnode@jbits.ca:boeing@ftp.jbits.ca/boeing$1/  --ftp-create-dirs
 #curl --ftp-ssl-reqd -k -T "../environment/env/main.php" ftp.jbits.ca/$1/ver/main-$(head -n 1 ../environment/currentversion.log).php -u boeing@jbits.ca:boeing
 #echo ...
 #echo File main-$(head -n 1 ../environment/currentversion.log).php has been uploaded to the $1 environment
