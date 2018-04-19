f320fb357b7b13992c19959b54f5e340740cdcfc
Julian Benavides <julianbenavides@gmail.com>
2018-04-19 02:08:53 -0700

    Update checkstatus.sh

--- scripts/checkstatus.sh
+++ scripts/checkstatus.sh
@@ -1,3 +1,9 @@
 # This script verifies the headers of a recently deployed environment; the header should be 200 or OK, if it shows something different it
 # Will notify the user
-echo $(curl --write-out %{http_code} --silent --output /dev/null https://jbits.ca/work/boeing/$1/)
+echo Establishing connection with the instance...
+echo curl http parameter: https://jbits.ca/work/boeing/$1/
+echo Requesting http headers...
+echo Response: $(curl --write-out %{http_code} --silent --output /dev/null https://jbits.ca/work/boeing/$1/)
+echo
+echo Starting automatic POST test to ensure that the microservice is working...
+echo curl --data "name=BoeingCandidate" https://jbits.ca/work/boeing/$1/
