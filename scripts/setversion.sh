7e927dd73626cae2f0d55ce932f0f40a3685364b
Julian Benavides <julianbenavides@gmail.com>
2018-04-18 23:28:29 -0700

    Update setversion.sh

--- scripts/setversion.sh
+++ scripts/setversion.sh
@@ -3,3 +3,6 @@ echo Checking current version...
 echo Reading from file: "../environment/currentversion.log"
 #VAR_1="$(head -n 1 ../environment/currentversion.log)"
 echo Current version: $(head -n 1 ../environment/currentversion.log)
+#version nomenclature RELEASE.UPDATE.PATCH starting with 0-0-1
+#Then I will explode the string to change the version depending if the deployment is in production -> release, test -> update, or development -> patch
+IFS=- read -a verArray <<< "$(head -n 1 ../environment/currentversion.log)"
