8b7f8e65c57c7a7cbfba6543619119e378d2c288
Julian Benavides <julianbenavides@gmail.com>
2018-04-18 22:37:56 -0700

    Update ftpsec.sh

--- scripts/ftpsec.sh
+++ scripts/ftpsec.sh
@@ -1,2 +1,2 @@
 # this script will upload the main block to any environment passed as the first parameter e.g. ftpsec.sh test
-curl --ftp-ssl-reqd -k -T "../environment/env/main.php" ftp.jbits.ca/$1/ver/main$(head -n 1 ../environment/currentversion.log).php -u boeing@jbits.ca:boeing
+curl --ftp-ssl-reqd -k -T "../environment/env/main.php" ftp.jbits.ca/$1/ver/main-$(head -n 1 ../environment/currentversion.log).php -u boeing@jbits.ca:boeing
