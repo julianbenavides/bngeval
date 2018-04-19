963b7dd3e8c8c5e1fd90b981db24fd4a14e1872e
Julian Benavides <julianbenavides@gmail.com>
2018-04-18 23:12:03 -0700

    Update setversion.sh

--- scripts/setversion.sh
+++ scripts/setversion.sh
@@ -1,5 +1,5 @@
 #script to set the new version of the main block after deployment in any environment
 echo Checking current version...
-echo Reading from file: ../environment/currentversion.log
+echo Reading from file: "../environment/currentversion.log"
 VAR1="$(head -n 1 ../environment/currentversion.log)"
 echo Current version: $(VAR1)
