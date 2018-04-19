672fc50c4c1b2e438a2612b145c1061d78a51fcd
Julian Benavides <julianbenavides@gmail.com>
2018-04-18 23:55:28 -0700

    Update setversion.sh

--- scripts/setversion.sh
+++ scripts/setversion.sh
@@ -31,4 +31,4 @@ echo Update: ${verArray[1]}
 echo Patch: ${verArray[2]} 
 echo
 echo Trying to rewrite the version files...
-sed -i "1s/.*/${verArray[0]}-${verArray[1]}-${verArray[2]}" ../environment/currentversion.log
+sed -i "1s/.*/'${verArray[0]}-${verArray[1]}-${verArray[2]}'" ../environment/currentversion.log
