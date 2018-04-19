fe8e8b0bf8cda9c773a7a0014e6f89002a626cfa
Julian Benavides <julianbenavides@gmail.com>
2018-04-19 03:23:54 -0700

    Update createnewnode.sh

--- scripts/createnewnode.sh
+++ scripts/createnewnode.sh
@@ -27,7 +27,28 @@ echo [done]
 echo ...
 echo Creating Files list on boeing$1
 echo ...
-
-#echo File main-$(head -n 1 ../environment/currentversion.log).php has been uploaded to the $1 environment
+echo Creating File list demo environment
+curl --ftp-ssl-reqd -k -T "../environment/index.php" ftp.jbits.ca/boeing$1/demo/index.php -u boeing@jbits.ca:boeing
+curl --ftp-ssl-reqd -k -T "../environment/track.log" ftp.jbits.ca/boeing$1/demo/track.log -u boeing@jbits.ca:boeing
+echo [done]
+echo Creating File list development environment
+curl --ftp-ssl-reqd -k -T "../environment/index.php" ftp.jbits.ca/boeing$1/development/index.php -u boeing@jbits.ca:boeing
+curl --ftp-ssl-reqd -k -T "../environment/track.log" ftp.jbits.ca/boeing$1/development/track.log -u boeing@jbits.ca:boeing
+echo [done]
+echo Creating File list production environment
+curl --ftp-ssl-reqd -k -T "../environment/index.php" ftp.jbits.ca/boeing$1/production/index.php -u boeing@jbits.ca:boeing
+curl --ftp-ssl-reqd -k -T "../environment/track.log" ftp.jbits.ca/boeing$1/production/track.log -u boeing@jbits.ca:boeing
+echo [done]
+echo Creating File list test environment
+curl --ftp-ssl-reqd -k -T "../environment/index.php" ftp.jbits.ca/boeing$1/test/index.php -u boeing@jbits.ca:boeing
+curl --ftp-ssl-reqd -k -T "../environment/track.log" ftp.jbits.ca/boeing$1/test/track.log -u boeing@jbits.ca:boeing
+echo [done]
+echo Creating Version Admin Page
+curl --ftp-ssl-reqd -k -T "../environment/env/adminversions.php" ftp.jbits.ca/boeing$1/index.php -u boeing@jbits.ca:boeing
+echo Ommiting master block
+echo ...
+echo New Node: boeing$1 has been successfully created.
+echo Exiting script
+echo
 #echo
 #echo End of transaction. Closing connection
