2bba8512dcf667960ce7baf619f5da9832cd8d81
Julian Benavides <julianbenavides@gmail.com>
2018-04-18 23:37:33 -0700

    Update setversion.sh

--- scripts/setversion.sh
+++ scripts/setversion.sh
@@ -6,5 +6,22 @@ echo Current version: $(head -n 1 ../environment/currentversion.log)
 #version nomenclature RELEASE.UPDATE.PATCH starting with 0-0-1
 #Then I will explode the string to change the version depending if the deployment is in production -> release, test -> update, or development -> patch
 IFS=- read -a verArray <<< "$(head -n 1 ../environment/currentversion.log)"
+
+#Here I will add a numeral to the current version depending on where was the code deployed; if it was in production
+# the release numeral will increase by one, if it was on test the update numeral will, if in development the patch numeral will.
+# the script will receive the parameter environment in variable 1
+if [ $1 = "Production" ]; then 
+    ${verArray[0]}=${verArray[0]}+1 
+elif [ $1 = "Test" ]; then
+    ${verArray[1]}=${verArray[1]}+1
+    elif [ $1 = "Development" ]; then
+      ${verArray[2]}=${verArray[2]}+1
+fi;
+
 echo DEBUG
-echo ${verArray[2]} 
+echo
+echo The deployment was performed on $1
+echo
+echo Release: ${verArray[0]} 
+echo Update: ${verArray[1]} 
+echo Patch: ${verArray[2]} 
