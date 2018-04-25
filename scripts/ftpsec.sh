3042815ae88489395333e51cb5f127cc017027dc
Julian Benavides <julianbenavides@gmail.com>
2018-04-25 16:41:06 -0700

    Update ftpsec.sh

--- scripts/ftpsec.sh
+++ scripts/ftpsec.sh
@@ -5,7 +5,7 @@ echo Passed parameter Environment --> $1
 echo ...
 echo Connecting through encrypted channel using ftps and tls 1.2.
 echo
-curl --ftp-ssl-reqd -k -T "../environment/env/main.php" ftp.jbits.ca/$1/ver/main-$(head -n 1 ../environment/currentversion.log).php -u boeing@jbits.ca:boeing
+curl --ftp-ssl-reqd -k -T "../environment/env/main.php" ftp.jbits.ca/$1/ver/main-$(head -n 1 ../environment/currentversion.log).php -u user@gomain:password
 echo ...
 echo File main-$(head -n 1 ../environment/currentversion.log).php has been uploaded to the $1 environment
 echo
